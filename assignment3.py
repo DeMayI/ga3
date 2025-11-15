00a5a257cadf9c40eee2530ea3fc24dfc507f3be
DeMayI <gigabyteruler@gmail.com>
2025-11-14 20:39:55 -0800

    Initial commit

---
+++ assignment3.py
@@ -0,0 +1,22 @@
+'''
+    This file contains the template for Assignment3. For testing it, I will place it
+    in a different directory, call the function <min_steps_flood_escape>, and check its output.
+    So, you can add/remove  whatever you want to/from this file. But, don't change the name
+    of the file or the name/signature of the following function.
+
+    I will use <python3> to run this code.
+'''
+
+def min_steps_flood_escape(grid: list[str]) -> int:
+    '''
+    Compute the minimum number of steps to reach the shelter, in a board specified by
+    a list of strings; grid[i] is the ith row of the grid.  grid[i][j] is the character
+    at the intersection of row i and column j.
+
+    @param grid: list[str] - representing the board.
+    Output: minium number of steps to reach from the initial point top-left to the
+    shelter bottom-right.
+    '''
+
+    # Your code here :)
+    return 0
